309d3dd179bc5f2fdfa9df30a78b79ac589899c0
Sudarshan Mane <104020229+sudarshanmane@users.noreply.github.com>
2024-06-24 10:51:45 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        santoshi: ["Satoshi", "sans-serif"],
+        santoshi: ["Santoshi", "sans-serif"],
       },
       boxShadow: {
         shadow_1: "rgba(0, 0, 0, 0.35) 0px 5px 15px;",
